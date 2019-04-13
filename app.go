94c02f3701397c77f4e772ebab3c0833271eaa56
Maxim Muzafarov <m.muzafarov@gmail.com>
2019-04-13 14:23:37 +0300

    Fix golint issues

--- app.go
+++ app.go
@@ -15,10 +15,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	http_host := flag.String("host", "", "HTTP_HOST")
-	remote_addr := flag.String("remote", "127.0.0.1", "remote addr")
+	httpHost := flag.String("host", "", "HTTP_HOST")
+	remoteAddr := flag.String("remote", "127.0.0.1", "remote addr")
 	modifier1 := flag.Int("modifier1", 0, "modifier1")
-	expected_status := flag.String("expected-status", "", "Fail if response status not equal")
+	expectedStatus := flag.String("expected-status", "", "Fail if response status not equal")
 	quiet := flag.Bool("q", false, "Disable output")
 	flag.Parse()
 	arg := flag.Arg(0)
@@ -30,12 +30,12 @@ func main() {
 		flag.Usage()
 	}
 	host, _, _ := net.SplitHostPort(url.Host)
-	if *http_host == "" {
-		http_host = &host
+	if *httpHost == "" {
+		httpHost = &host
 	}
-	response := get(url, *http_host, *remote_addr, *modifier1)
-	if *expected_status != "" {
-		os.Exit(int(check_status(response, *expected_status)))
+	response := get(url, *httpHost, *remoteAddr, *modifier1)
+	if *expectedStatus != "" {
+		os.Exit(int(checkStatus(response, *expectedStatus)))
 	}
 	if !*quiet {
 		fmt.Print(string(response))
--- uwsgi.go
+++ uwsgi.go
@@ -9,17 +9,17 @@ import (
 	"regexp"
 )
 
-func encode_size(message []byte) []byte {
-	var_size := make([]byte, 2)
-	binary.LittleEndian.PutUint16(var_size, uint16(len(message)))
-	return var_size
+func encodeSize(message []byte) []byte {
+	size := make([]byte, 2)
+	binary.LittleEndian.PutUint16(size, uint16(len(message)))
+	return size
 }
 
-func uwsgi_pack(path, query, host, remote_addr string, modifier1 int) []byte {
+func uwsgiPack(path, query, host, remoteAddr string, modifier1 int) []byte {
 	if path == "" {
 		path = "/"
 	}
-	uwsgi_params := map[string]string{
+	uwsgiParams := map[string]string{
 		"SERVER_PROTOCOL": "HTTP/1.1",
 		"REQUEST_METHOD":  "GET",
 		"PATH_INFO":       path,
@@ -27,37 +27,37 @@ func uwsgi_pack(path, query, host, remote_addr string, modifier1 int) []byte {
 		"QUERY_STRING":    query,
 		"SERVER_NAME":     host,
 		"HTTP_HOST":       host,
-		"REMOTE_ADDR":     remote_addr,
+		"REMOTE_ADDR":     remoteAddr,
 	}
 	var params []byte
-	for k, v := range uwsgi_params {
-		bytes_k, bytes_v := []byte(k), []byte(v)
-		params = append(append(params, encode_size(bytes_k)...), bytes_k...)
-		params = append(append(params, encode_size(bytes_v)...), bytes_v...)
+	for k, v := range uwsgiParams {
+		bytesKey, bytesValue := []byte(k), []byte(v)
+		params = append(append(params, encodeSize(bytesKey)...), bytesKey...)
+		params = append(append(params, encodeSize(bytesValue)...), bytesValue...)
 	}
-	return append(append(append([]byte{byte(modifier1)}, encode_size(params)...), 0), params...)
+	return append(append(append([]byte{byte(modifier1)}, encodeSize(params)...), 0), params...)
 }
 
-func get(url *url.URL, http_host, remote_addr string, modifier1 int) []byte {
-	uwsgi_request := uwsgi_pack(url.Path, url.RawQuery, http_host, remote_addr, modifier1)
+func get(url *url.URL, httpHost, remoteAddr string, modifier1 int) []byte {
+	uwsgiRequest := uwsgiPack(url.Path, url.RawQuery, httpHost, remoteAddr, modifier1)
 	conn, err := net.Dial("tcp", url.Host)
 	if err != nil {
 		return []byte("No connection")
 	}
-	conn.Write(uwsgi_request)
+	conn.Write(uwsgiRequest)
 	var response bytes.Buffer
 	io.Copy(&response, conn)
 	defer conn.Close()
 	return response.Bytes()
 }
 
-func check_status(response []byte, expected_status string) int {
-	response_re := regexp.MustCompile(`^HTTP/[01].[01] ([0-9]{3}) `)
-	matches := response_re.FindSubmatch(response)
+func checkStatus(response []byte, expectedStatus string) int {
+	responseRe := regexp.MustCompile(`^HTTP/[01].[01] ([0-9]{3}) `)
+	matches := responseRe.FindSubmatch(response)
 	if len(matches) < 2 {
 		return 1
 	}
-	if string(matches[1]) == expected_status {
+	if string(matches[1]) == expectedStatus {
 		return 0
 	}
 	return 1
